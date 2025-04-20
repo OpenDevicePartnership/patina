dbc364590123d450c5517ff9db05dd6ae625c205
Daniel Kouchekinia <kouchekiniad@microsoft.com>
2025-04-20 16:40:13 -0700

    Revert "Use volatile writes to write Adv Logger messages"
    This reverts commit c55a7a754b6adf43468bf263be59b89e3eea61be.

--- adv_logger/src/memory_log.rs
+++ adv_logger/src/memory_log.rs
@@ -11,8 +11,8 @@
 //!
 use core::{
     ffi::c_void,
-    mem::{self, size_of},
-    ptr::{self, write_volatile},
+    mem::size_of,
+    ptr,
     sync::atomic::{AtomicU32, Ordering},
 };
 use r_efi::efi;
@@ -273,9 +273,7 @@ impl AdvLoggerMessageEntry {
 
         // write the data.
         let message = adv_entry.offset(1) as *mut u8;
-        for byte_index in 0..log_entry.data.len() {
-            ptr::write_volatile(message.wrapping_add(byte_index), log_entry.data[byte_index]);
-        }
+        ptr::copy(log_entry.data.as_ptr(), message, log_entry.data.len());
 
         adv_entry.as_ref()
     }
