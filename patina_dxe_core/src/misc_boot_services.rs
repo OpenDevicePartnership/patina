e3a9f419b590fb00fb695f2591472429d73a6534
Rathi Kartheek <rkartheek@microsoft.com>
2025-07-16 10:46:09 -0700

    Addressing John's comments

--- patina_dxe_core/src/misc_boot_services.rs
+++ patina_dxe_core/src/misc_boot_services.rs
@@ -24,6 +24,7 @@ use crate::{
     systemtables::{EfiSystemTable, SYSTEM_TABLE},
     GCD,
 };
+//use mu_pi::{dxe_services, fw_fs::FirmwareVolume};
 
 static METRONOME_ARCH_PTR: AtomicPtr<protocols::metronome::Protocol> = AtomicPtr::new(core::ptr::null_mut());
 static WATCHDOG_ARCH_PTR: AtomicPtr<protocols::watchdog::Protocol> = AtomicPtr::new(core::ptr::null_mut());
@@ -40,7 +41,6 @@ pub const PRE_EBS_GUID: efi::Guid =
     efi::Guid::from_fields(0x5f1d7e16, 0x784a, 0x4da2, 0xb0, 0x84, &[0xf8, 0x12, 0xf2, 0x3a, 0x8d, 0xce]);
 
 // TODO [END]: LOCAL (TEMP) GUID DEFINITIONS (MOVE LATER)
-#[cfg(not(tarpaulin_include))]
 extern "efiapi" fn calculate_crc32(data: *mut c_void, data_size: usize, crc_32: *mut u32) -> efi::Status {
     if data.is_null() || data_size == 0 || crc_32.is_null() {
         return efi::Status::INVALID_PARAMETER;
@@ -54,7 +54,6 @@ extern "efiapi" fn calculate_crc32(data: *mut c_void, data_size: usize, crc_32: 
     efi::Status::SUCCESS
 }
 
-#[cfg(not(tarpaulin_include))]
 pub fn core_install_configuration_table(
     vendor_guid: efi::Guid,
     vendor_table: Option<&mut c_void>,
@@ -138,7 +137,6 @@ pub fn core_install_configuration_table(
     Ok(())
 }
 
-#[cfg(not(tarpaulin_include))]
 extern "efiapi" fn install_configuration_table(table_guid: *mut efi::Guid, table: *mut c_void) -> efi::Status {
     if table_guid.is_null() {
         return efi::Status::INVALID_PARAMETER;
@@ -158,7 +156,6 @@ extern "efiapi" fn install_configuration_table(table_guid: *mut efi::Guid, table
 
 // Induces a fine-grained stall. Stalls execution on the processor for at least the requested number of microseconds.
 // Execution of the processor is not yielded for the duration of the stall.
-#[cfg(not(tarpaulin_include))]
 extern "efiapi" fn stall(microseconds: usize) -> efi::Status {
     let metronome_ptr = METRONOME_ARCH_PTR.load(Ordering::SeqCst);
     if let Some(metronome) = unsafe { metronome_ptr.as_mut() } {
@@ -192,7 +189,7 @@ extern "efiapi" fn stall(microseconds: usize) -> efi::Status {
 //
 // The watchdog timer is only used during boot services. On successful completion of
 // EFI_BOOT_SERVICES.ExitBootServices() the watchdog timer is disabled.
-#[cfg(not(tarpaulin_include))]
+
 extern "efiapi" fn set_watchdog_timer(
     timeout: usize,
     _watchdog_code: u64,
@@ -213,9 +210,9 @@ extern "efiapi" fn set_watchdog_timer(
     }
 }
 
+#[cfg(not(tarpaulin_include))]
 // This callback is invoked when the Metronome Architectural protocol is installed. It initializes the
 // METRONOME_ARCH_PTR to point to the Metronome Architectural protocol interface.
-#[cfg(not(tarpaulin_include))]
 extern "efiapi" fn metronome_arch_available(event: efi::Event, _context: *mut c_void) {
     match PROTOCOL_DB.locate_protocol(protocols::metronome::PROTOCOL_GUID) {
         Ok(metronome_arch_ptr) => {
@@ -228,9 +225,9 @@ extern "efiapi" fn metronome_arch_available(event: efi::Event, _context: *mut c_
     }
 }
 
+#[cfg(not(tarpaulin_include))]
 // This callback is invoked when the Watchdog Timer Architectural protocol is installed. It initializes the
 // WATCHDOG_ARCH_PTR to point to the Watchdog Timer Architectural protocol interface.
-#[cfg(not(tarpaulin_include))]
 extern "efiapi" fn watchdog_arch_available(event: efi::Event, _context: *mut c_void) {
     match PROTOCOL_DB.locate_protocol(protocols::watchdog::PROTOCOL_GUID) {
         Ok(watchdog_arch_ptr) => {
@@ -242,7 +239,7 @@ extern "efiapi" fn watchdog_arch_available(event: efi::Event, _context: *mut c_v
         Err(err) => panic!("Unable to retrieve watchdog arch: {:?}", err),
     }
 }
-#[cfg(not(tarpaulin_include))]
+
 pub extern "efiapi" fn exit_boot_services(_handle: efi::Handle, map_key: usize) -> efi::Status {
     static EXIT_BOOT_SERVICES_CALLED: AtomicBool = AtomicBool::new(false);
 
@@ -354,14 +351,149 @@ pub fn init_misc_boot_services_support(bs: &mut efi::BootServices) {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::misc_boot_services;
     use crate::systemtables;
+    use core::{ffi::c_void, ptr, str::FromStr};
+    use mu_pi::protocols::status_code::EfiStatusCodeData;
+    use r_efi::efi;
     use r_efi::efi::{BootServices, Status};
-    // Run Test cases with the `cargo test` command.
+    use std::cell::UnsafeCell;
+
+    // Define a global static variable to store the Boot Services pointer
+    struct BootServicesWrapper {
+        boot_services: UnsafeCell<Option<&'static mut efi::BootServices>>,
+    }
+
+    unsafe impl Sync for BootServicesWrapper {}
+
+    static BOOT_SERVICES: BootServicesWrapper = BootServicesWrapper { boot_services: UnsafeCell::new(None) };
+
+    // Function to initialize the global Boot Services pointer
+    pub fn initialize_boot_services(bs: &mut efi::BootServices) {
+        unsafe {
+            *BOOT_SERVICES.boot_services.get() = Some(&mut *(bs as *mut _));
+        }
+    }
+
+    pub fn get_boot_services() -> Option<&'static mut efi::BootServices> {
+        unsafe { (*BOOT_SERVICES.boot_services.get()).as_mut().map(|bs| &mut **bs) }
+    }
     #[test]
     fn test_init_misc_boot_services_support() {
         let mut st = systemtables::SYSTEM_TABLE.lock();
         let st = st.as_mut().expect("System Table not initialized!");
+
+        // Initialize BOOT_SERVICES using the BootServices instance from SYSTEM_TABLE
+        initialize_boot_services(st.boot_services_mut());
         init_misc_boot_services_support(st.boot_services_mut());
+    }
+
+    #[test]
+    fn test_misc_calc_crc32() {
+        let mut st = systemtables::SYSTEM_TABLE.lock();
+        let st = st.as_mut().expect("System Table not initialized!");
+
+        // Initialize BOOT_SERVICES using the BootServices instance from SYSTEM_TABLE
+        initialize_boot_services(st.boot_services_mut());
+        init_misc_boot_services_support(st.boot_services_mut());
+
+        static BUFFER: [u8; 16] = [0; 16];
+        let mut data_crc: u32 = 0;
+        (st.boot_services_mut().calculate_crc32)(
+            BUFFER.as_ptr() as *mut c_void,
+            BUFFER.len(),
+            &mut data_crc as *mut u32,
+        );
+
+        (st.boot_services_mut().calculate_crc32)(BUFFER.as_ptr() as *mut c_void, 0, &mut data_crc as *mut u32);
+    }
+    #[test]
+    fn test_misc_watchdog_timer() {
+        let mut st = systemtables::SYSTEM_TABLE.lock();
+        let st = st.as_mut().expect("System Table not initialized!");
+
+        // Initialize BOOT_SERVICES using the BootServices instance from SYSTEM_TABLE
+        initialize_boot_services(st.boot_services_mut());
+        init_misc_boot_services_support(st.boot_services_mut());
+
+        (st.boot_services_mut().set_watchdog_timer)(300, 0, 0, ptr::null_mut());
+        (st.boot_services_mut().set_watchdog_timer)(0, 0, 0, ptr::null_mut()); //nothing changed.
+
+        let data: [efi::Char16; 6] = [b'H' as u16, b'e' as u16, b'l' as u16, b'l' as u16, b'o' as u16, 0];
+        let data_ptr = data.as_ptr() as *mut efi::Char16;
+        // Case 1: Set the watchdog timer with non-null data
+        let status = (st.boot_services_mut().set_watchdog_timer)(300, 0, data.len(), data_ptr);
+
+        // Case 2: Disable the watchdog timer with non-null data
+        let status = (st.boot_services_mut().set_watchdog_timer)(0, 0, data.len(), data_ptr);
+    }
+    #[test]
+    fn test_misc_stall() {
+        let mut st = systemtables::SYSTEM_TABLE.lock();
+        let st = st.as_mut().expect("System Table not initialized!");
+
+        // Initialize BOOT_SERVICES using the BootServices instance from SYSTEM_TABLE
+        initialize_boot_services(st.boot_services_mut());
+        init_misc_boot_services_support(st.boot_services_mut());
+
+        (st.boot_services_mut().stall)(10000);
+        (st.boot_services_mut().stall)(0); // Changed
+        (st.boot_services_mut().stall)(usize::MAX); // Changed
+    }
+
+    #[test]
+    fn test_misc_install_configuration_table() {
+        // Acquire the lock on SYSTEM_TABLE
+        let mut st_guard = systemtables::SYSTEM_TABLE.lock();
+        let st = st_guard.as_mut().expect("System Table not initialized!");
+        // Prepare parameters
+        let table_guid = Box::into_raw(Box::new(efi::Guid::from_fields(
+            0x12345678,
+            0x1234,
+            0x5678,
+            0x12,
+            0x34,
+            &[0x56, 0x78, 0x9a, 0xbc, 0xde, 0xf0],
+        )));
+        let table_data: [u8; 16] = [0xAA; 16]; // Example data
+        let table_ptr = Box::into_raw(Box::new(table_data)) as *mut c_void;
+        // Release the lock before calling install_configuration_table
+        std::mem::drop(st_guard);
+        // Call install_configuration_table
+        let status = install_configuration_table(table_guid, table_ptr);
+        assert_eq!(
+            status,
+            efi::Status::SUCCESS,
+            "Expected SUCCESS when installing configuration table with valid parameters"
+        );
+        // Call install_configuration_table with second parameter null
+        let status = install_configuration_table(table_guid, core::ptr::null_mut());
+        // Call install_configuration_table with both parameters null
+        let status = install_configuration_table(core::ptr::null_mut(), core::ptr::null_mut());
+        // Clean up
+        unsafe {
+            Box::from_raw(table_guid); // Free the allocated memory for `table_guid`
+            Box::from_raw(table_ptr as *mut [u8; 16]); // Free the allocated memory for `table`
+        }
+    }
+
+    #[test]
+    fn test_misc_exit_boot_services() {
+        let valid_map_key: usize = 0x2000;
+        // Define a mock function that matches the type of TERMINATE_MEMORY_MAP_FN
+        fn test_terminate_memory_map(key: usize) -> Result<(), EfiError> {
+            assert_eq!(key, 0x2000, "Expected valid map_key");
+            Ok(())
+        }
+        // Assign the mock function to TERMINATE_MEMORY_MAP_FN
+        unsafe {
+            let TERMINATE_MEMORY_MAP_FN = test_terminate_memory_map;
+        }
+        // Acquire the lock on SYSTEM_TABLE
+        let mut st_guard = systemtables::SYSTEM_TABLE.lock();
+        let st = st_guard.as_mut().expect("System Table not initialized!");
+        init_misc_boot_services_support(st.boot_services_mut());
+        // Call exit_boot_services with a valid map_key
+        let handle: efi::Handle = 0x1000 as efi::Handle; // Example handle
+        let status = (st.boot_services_mut().exit_boot_services)(handle, valid_map_key);
     }
 }
