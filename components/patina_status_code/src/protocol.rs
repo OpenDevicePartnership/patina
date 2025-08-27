e51bd9048dca87b62d1ecf310a5787a01f041b6e
Sherry Fan <sherryfan@microsoft.com>
2025-08-26 18:57:20 -0700

    add note for me

--- components/patina_status_code/src/protocol.rs
+++ components/patina_status_code/src/protocol.rs
@@ -12,3 +12,5 @@ pub const REGISTER_UNREGISTER_PROTOCOL_GUID: efi::Guid =
     efi::Guid::from_fields(0x86212936, 0xe76, 0x41c8, 0xa0, 0x3a, &[0x2a, 0xf2, 0xfc, 0x1c, 0x39, 0xe2]);
 
 // Register and unregister
+
+// sherry: u probably need a global :(
