a494863d013e1cf7832be9f48cda6189797209e2
Ron Gurr <ron.gurr@microsoft.com>
2026-01-16 11:21:48 -0800

    Update to allow register_protocol_notify to return already installed protocol handles

--- sdk/patina/src/boot_services.rs
+++ sdk/patina/src/boot_services.rs
@@ -1258,9 +1258,19 @@ impl BootServices for StandardBootServices {
             _ => ptr::null_mut(),
         };
 
-        // Use to get the buffer_size
+        // Expect locate to return BUFFER_TOO_SMALL to get the buffer_size
         let mut buffer_size = 0;
-        locate_handle(search_type.into(), protocol, search_key, ptr::addr_of_mut!(buffer_size), ptr::null_mut());
+        match locate_handle(
+            search_type.into(),
+            protocol,
+            search_key,
+            ptr::addr_of_mut!(buffer_size),
+            ptr::null_mut(),
+        ) {
+            s if s == efi::Status::BUFFER_TOO_SMALL => (),
+            s if s.is_error() => return Err(s),
+            _ => return Err(efi::Status::PROTOCOL_ERROR),
+        }
 
         let buffer = self.allocate_pool(EfiMemoryType::BootServicesData, buffer_size)?;
 
